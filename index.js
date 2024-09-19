f53274f689c847596e684205f822a433621b9543
Kian Holden <kian@holden.id.au>
2024-09-19 14:51:50 -0400

    test2

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const app = require("express")();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+// const app = require("express")();
+import express from 'express'
+const app = express()
+import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+    
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: { origin: '*' } });
+
 let count=1
 
 app.get("/", (req, res) => {
@@ -18,6 +28,6 @@ io.on("connection", function(socket) {
 
 
 
-http.listen(3000, () => 
+httpServer.listen(3000, () => 
     console.log("listening on http://localhost:3000")
 )
